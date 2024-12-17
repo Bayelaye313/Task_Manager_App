07052260a7afd88ff0878ba0bd90318da209cd1e
bayelaye313 <bayelaye0510@gmail.com>
2024-12-17 00:03:26 +0000

    userregister

--- backend/src/controllers/goalsControllers.js
+++ backend/src/controllers/goalsControllers.js
@@ -2,11 +2,11 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModels");
 
-const getGoal = asyncHandler(async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   const users = await User.find();
   res.status(200).json(users);
 });
-const setUser = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, photo, bio, role, isVerified } = req.body;
 
   if (!name || !email || !password) {
@@ -15,7 +15,16 @@ const setUser = asyncHandler(async (req, res) => {
       "Please provide all required fields: name, email, password"
     );
   }
-
+  //check password length
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password length must be at least 6 characters");
+  }
+  //check if user already exist
+  const userExisted = User.findOne({ email });
+  if (userExisted) {
+    return res.status(400).json("user already exist");
+  }
   const user = await User.create({
     name,
     email,
@@ -25,14 +34,28 @@ const setUser = asyncHandler(async (req, res) => {
     role,
     isVerified,
   });
+  if (user) {
+    const { name, email, password, photo, bio, role, isVerified } = user;
+    res.status(201).json({
+      name,
+      email,
+      password,
+      photo,
+      bio,
+      role,
+      isVerified,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
 
   res.status(200).json(user);
 });
 
-const updateGoal = asyncHandler(async (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
-  // Check if goal exists
   if (!user) {
     res.status(400);
     throw new Error("Goal with this ID not found");
@@ -42,28 +65,28 @@ const updateGoal = asyncHandler(async (req, res) => {
 
   const updatedGoal = await User.findByIdAndUpdate(
     req.params.id,
-    { text: req.body.text }, // Only update the text field explicitly
+    { text: req.body.text },
     { new: true, runValidators: true }
   );
 
   res.status(200).json(updatedGoal);
 });
 
-const deleteGoal = asyncHandler(async (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (!user) {
     res.status(400);
     throw new Error("can not find error");
   }
-  const deletedGoal = await Goal.findByIdAndDelete(req.params.id, {
+  const deletedUser = await User.findByIdAndDelete(req.params.id, {
     new: true,
   });
-  res.status(200).json(deletedGoal);
+  res.status(200).json(deletedUser);
 });
 
 module.exports = {
-  getGoal,
-  setUser,
-  updateGoal,
-  deleteGoal,
+  getUser,
+  registerUser,
+  updateUser,
+  deleteUser,
 };
--- backend/src/models/userModels.js
+++ backend/src/models/userModels.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// Define schema
 const UserSchema = mongoose.Schema(
   {
     name: {
