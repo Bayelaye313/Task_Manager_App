fd60d25c9baa9e77d52b9d2eef66b69235d3e325
bayelaye313 <bayelaye0510@gmail.com>
2025-01-19 18:57:03 +0000

    fix cross-site

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -62,7 +62,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: "/",
     });
